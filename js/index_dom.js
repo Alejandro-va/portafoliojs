cc4beb65130b462ba3782ac01d8dd1c84688b135
Alejandro-va <prof.jahz@gmail.com>
2021-05-12 17:59:29 -0300

    menu

--- js/index_dom.js
+++ js/index_dom.js
@@ -3,6 +3,7 @@ import piedarPapelTijera from "./piedraPapelTijera.js";
 import efectoTexto from "./efectoTexto.js";
 import maquinaDEscribir from "./maquinaDEscribir.js";
 import darkTheme from "./darkTheme.js";
+import hamburgerMenu from "./menu_hamburgesa.js";
 
 
 document.addEventListener('DOMContentLoaded', (e)=>{
@@ -15,4 +16,7 @@ document.addEventListener('DOMContentLoaded', (e)=>{
    maquinaDEscribir('encabezado', 'subtitulo');
 
    darkTheme("#theme-button");
+
+  //menu hamburgesa
+  hamburgerMenu(".panel-btn", ".panel", ".menu a");
 })
